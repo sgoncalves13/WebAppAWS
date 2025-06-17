dae6ebb8420c4a9f7da98393e010a9a3f1febcb8
sgoncalves13 <s.goncalves@uniandes.edu.co>
2025-06-17 15:14:18 -0500

    s

--- models/index.js
+++ models/index.js
@@ -1,10 +1,9 @@
 const Sequelize = require('sequelize');
 const config = require('../config/config.js')['development'];
-console.log(process.env.DB_USER, process.env.DB_PASSWORD);
+console.log(process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_HOST);
 console.log(config.username, config.password);
 
 require('dotenv').config();
-const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
