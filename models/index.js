fa4427857d2a03cc92dac1b2505bac4ed661cb12
sgoncalves13 <s.goncalves@uniandes.edu.co>
2025-06-17 15:20:44 -0500

    r

--- models/index.js
+++ models/index.js
@@ -3,7 +3,16 @@ const config = require('../config/config.js')['development'];
 console.log(process.env.DB_USER, process.env.DB_PASSWORD);
 console.log(config.username, config.password);
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres'
+  }
+);
 
 sequelize.authenticate()
   .then(() => console.log('✅ Connected!'))
