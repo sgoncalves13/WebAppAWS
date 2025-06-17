fd9535d90228f3182ed25ff4ec820433e7891cce
sgoncalves13 <s.goncalves@uniandes.edu.co>
2025-06-17 15:19:27 -0500

    e

--- models/index.js
+++ models/index.js
@@ -1,30 +1,12 @@
 const Sequelize = require('sequelize');
 const config = require('../config/config.js')['development'];
-console.log(process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_HOST);
+console.log(process.env.DB_USER, process.env.DB_PASSWORD);
 console.log(config.username, config.password);
 
-require('dotenv').config();
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  }
-);
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 sequelize.authenticate()
   .then(() => console.log('✅ Connected!'))
   .catch((error) => console.error('❌ Connection failed:', error));
-
 
 module.exports = sequelize;
