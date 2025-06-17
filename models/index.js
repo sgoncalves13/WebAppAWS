8b6a16c95763f5a51638e52c4423fb67a47618c2
sgoncalves13 <s.goncalves@uniandes.edu.co>
2025-06-17 13:39:19 -0500

    test

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize');
 const config = require('../config/config.js')['development'];
+console.log(process.env.DB_USER, process.env.DB_PASSWORD);
+console.log(config.username, config.password);
+
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 module.exports = sequelize;
