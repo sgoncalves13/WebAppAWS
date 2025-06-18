d771a64acd2dfd504a2aab369dd152d210ab0be1
sgoncalves13 <s.goncalves@uniandes.edu.co>
2025-06-18 11:42:41 -0500

    fix

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const PORT = 3000;
 
 app.use(routes);
 
-const cors = require('cors');
-
-
-// CORS para permitir desde tu frontend (ej. localhost:3000)
 app.use(cors({
   origin: 'http://localhost:3000'
 }));
