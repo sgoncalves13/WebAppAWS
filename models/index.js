bb66f1b7a133254dcef3383d01d1e4bdafbc8143
sgoncalves13 <s.goncalves@uniandes.edu.co>
2025-06-18 10:13:04 -0500

    a

--- models/index.js
+++ models/index.js
@@ -10,13 +10,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
+    dialect: 'postgres'
   }
 );
 
